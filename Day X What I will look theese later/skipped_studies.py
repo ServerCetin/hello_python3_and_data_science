9c53404c19db78dfe3fa6f4fd0870bdaf41e14c9
servercetin <servercetin@hotmail.com>
2022-04-17 17:13:02 +0300

    learned file handling

--- Day X What I will look theese later/skipped_studies.py
+++ Day X What I will look theese later/skipped_studies.py
@@ -1,5 +1,4 @@
 # I will look at them later :)
 
 # 30 Days Of Python: Day 15 Datetime, Exception, Packing, Unpacking, Zip, Enum - Higher Order Functions
-# 30 Days Of Python: Day 19 - File Handling
 # 30 Days Of Python: Day 18 - Regular Expressions
