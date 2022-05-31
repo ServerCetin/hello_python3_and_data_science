f7089884ae465b55c017595a3b3cc5b221a17685
Server ÇETİN <servercetin@hotmail.com>
2022-05-31 19:55:50 +0300

    Day 20 add

---
+++ Day 20 - Joining Data with pandas/2- Merging Tables with Different Join Types.py
@@ -0,0 +1,92 @@
+# Left Join: same syntax with inner join but with how='left' parameter
+# Right Join: same syntax with inner join but with how='right' parameter
+
+'''
+# Merge the movies table with the financials table with a left join
+movies_financials = movies.merge(financials, on='id', how='left')
+
+# Count the number of rows in the budget column that are missing
+number_of_missing_fin = movies_financials['budget'].isnull().sum()
+
+# Print the number of movies missing financials
+print(number_of_missing_fin)
+'''
+
+'''
+# Merge the toy_story and taglines tables with a inner join
+toystory_tag = toy_story.merge(taglines, on='id', how='inner')
+
+# Print the rows and shape of toystory_tag
+print(toystory_tag)
+print(toystory_tag.shape)
+'''
+
+# if on arg names is not same on 2 table we use this args
+# tv_movies = movies.merge(tv_genre, how='right', left_on='id', right_on='movie_id')
+
+# movies_financials = movies.merge(financials, on='id', how='outer')
+
+'''
+# Merge action_movies to the scifi_movies with right join
+action_scifi = action_movies.merge(scifi_movies, on='movie_id', how='right',
+                                   suffixes=('_act','_sci'))
+
+# From action_scifi, select only the rows where the genre_act column is null
+scifi_only = action_scifi[action_scifi['genre_act'].isnull()]
+
+# Merge the movies and scifi_only tables with an inner join
+movies_and_scifi_only = movies.merge(scifi_only, how='inner', left_on='id',right_on='movie_id')
+
+# Print the first few rows and shape of movies_and_scifi_only
+print(movies_and_scifi_only.head())
+print(movies_and_scifi_only.shape)
+'''
+
+'''
+# Use right join to merge the movie_to_genres and pop_movies tables
+genres_movies = movie_to_genres.merge(pop_movies, how='right', right_on='id', left_on='movie_id')
+
+# Count the number of genres
+genre_count = genres_movies.groupby('genre').agg({'id':'count'})
+
+# Plot a bar chart of the genre_count
+genre_count.plot(kind='bar')
+plt.show()
+'''
+
+'''
+# self join
+
+# Merge the crews table to itself
+crews_self_merged = crews.merge(crews, on='id', how='inner',
+                                suffixes=('_dir','_crew'))
+
+# Create a boolean index to select the appropriate rows
+boolean_filter = ((crews_self_merged['job_dir'] == 'Director') & 
+                  (crews_self_merged['job_crew'] != 'Director'))
+direct_crews = crews_self_merged[boolean_filter]
+
+# Print the first few rows of direct_crews
+print(direct_crews.head())
+'''
+
+
+
+'''
+# Merge sequels and financials on index id
+sequels_fin = sequels.merge(financials, on='id', how='left')
+
+# Self merge with suffixes as inner join with left on sequel and right on id
+orig_seq = sequels_fin.merge(sequels_fin, how='inner', left_on='sequel', 
+                             right_on='id', right_index=True,
+                             suffixes=('_org','_seq'))
+
+# Add calculation to subtract revenue_org from revenue_seq 
+orig_seq['diff'] = orig_seq['revenue_seq'] - orig_seq['revenue_org']
+
+# Select the title_org, title_seq, and diff 
+titles_diff = orig_seq[['title_org','title_seq','diff']]
+
+# Print the first rows of the sorted titles_diff
+print(titles_diff.sort_values(by='diff', ascending=False).head())
+'''
---
+++ Day 20 - Joining Data with pandas/3- Advanced Merging and Concatenating.py
@@ -0,0 +1,12 @@
+# filtering joins
+
+'''
+semi join
+    -returns the intersection, similar to an inner join
+    -returns only tables from left table, not right table
+    -no duplicates
+
+anti join
+    -returns the left table, excluding the intersection
+    -returns only columns from the left table
+'''
