8aa0e9454b176580e57e40011d05e2c7551f181e
Server ÇETİN <servercetin@hotmail.com>
2022-07-13 17:00:06 +0300

    skipped studies update

--- Day X What I will look these later/skipped_studies.py
+++ Day X What I will look these later/skipped_studies.py
@@ -2,3 +2,4 @@
 
 # 30 Days Of Python: Day 15 Datetime, Exception, Packing, Unpacking, Zip, Enum - Higher Order Functions
 # 30 Days Of Python: Day 18 - Regular Expressions
+# Decorator and Closure func  https://app.patika.dev/courses/ileri-python/decorator
